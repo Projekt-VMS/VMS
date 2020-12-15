32d684b6c5fe386254440097c8901ba2f8f33912
Klaus <klaus-fischer96@gmx.de>
2020-12-15 14:56:48 +0100

    erster Angular Versuch --> Sidebar

--- vms/src/app/app.module.ts
+++ vms/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -13,7 +14,8 @@ import { faFilm } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
