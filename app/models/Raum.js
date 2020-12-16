1924e206cd3c6fbfaf1f4c236a8e90633c46eb52
dorianprobstmeier <dorian@probstmeier.de>
2020-12-16 21:32:21 +0100

    Update Raum.js

--- app/models/Raum.js
+++ app/models/Raum.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var validate = require('mongoose-validator');
+var autoIncrement = require('mongoose-auto-increment');
+
 mongoose.set('useCreateIndex', true);
 
 // Raumgröße
@@ -10,9 +12,10 @@ var sizeValidator = {
 }
 
 var raumSchema = mongoose.Schema({
+    rid: {type: Number},
     kapazitaet: {type: Number, required: true, validate: sizeValidator},
     raumpreis: {type: Number, required: true},
 }, {collection : "Raum"});
 
-
+raumSchema.plugin(autoIncrement, {inc_field: 'rid'});
 module.exports = mongoose.model('Raum', raumSchema);
