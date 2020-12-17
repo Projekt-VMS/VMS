73d18344d06dadd0b1c3dcda63bf122c2160514b
dorianprobstmeier <dorian@probstmeier.de>
2020-12-17 11:31:25 +0100

    Teilnehmer controller + raumcontroller

--- app/controllers/raum.controller.js
+++ app/controllers/raum.controller.js
@@ -1,44 +1,63 @@
-const raumSchema = require("mongodb/lib/collection");
-var mongoose = require('mongoose');
+const express = require('express'),
+    raumController = express.Router();
 
-class Raum {
-    constructor(kapazitaet, raumpreis) {
-        this.kapazitaet = kapazitaet;
-        this.raumpreis = raumpreis;
-    }
-    create(kapazitaet, raumpreis){
 
-        let raumInstance = new Raum(kapazitaet, raumpreis);
+let Raum = require('../models/Raum');
 
-        raumInstance.save()
-                .catch(err=>{
-                console.log(err.toString());
-                res.status(500).send(err.toString()); })
-                .then(dbres=>{
+//create
 
-                    console.log(dbres);
-                    res.json(dbres);
-                });
-    }
+raumController.route('/add').post(function (req, res) {
 
-    show(){
-        raumSchema.find(function(err, data) {
-            if(err){
-                console.log(err);
-            }
-            else{
-                res.send(data);
-            }
+    let raumInstance = new Raum(req.body);
+    console.log(raumInstance);
+    raumInstance.save()
+        .then(raumInstance => {
+            res.status(200).json({ 'Success': true })
+        })
+        .catch(err => {
+            res.status(400).send({ 'Success': false, 'Message': 'Error occured while creating new user' });
         });
-    }
 
-    edit(){
-        raumSchema.findAndModify()
-    }
+});
 
-    delete(){
+//delete
+raumController.route('/delete/:id').get(function (req, res) {
 
-    }
+    let raumId = req.params.id;
 
-}
+    Raum.find({ 'Raum.rid': raumId }).remove(function (err, raum) {
+        if (err)
+            res.json({ 'Success': false, 'Message': 'Raum not found' });
+        else
+            res.json({ 'Success': true });
+    });
+});
+
+
+// update
+raumController.route('/edit/:id').post(function (req, res) {
+
+    let raumId = req.params.id;
+
+    Raum.findOne({ 'Raum.rid': raumId }, function (err, raum) {
+        if (!raum)
+            return next(new Error('user not found'));
+        else {
+            raum.kapazitaet = req.body.kapazitaet;
+            raum.raumpreis = req.body.raumpreis;
+
+
+            raum.save().then(raum => {
+                res.json({ 'Success': true });
+            })
+                .catch(err => {
+                    res.status(400).json({ 'Success': false });
+                });
+        }
+    });
+});
+
+
+
+module.exports = raumController;
 
