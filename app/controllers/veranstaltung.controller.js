47d613d55f4902296168b0696217ec5f2122d4c5
Klaus <klaus-fischer96@gmx.de>
2020-12-22 14:33:56 +0100

    Mailversand

--- app/controllers/veranstaltung.controller.js
+++ app/controllers/veranstaltung.controller.js
@@ -1,5 +1,6 @@
 const express = require('express'),
     veranstaltungsController = express();
+const nodemailer = require('nodemailer')
 
 let Veranstaltung = require('../models/Veranstaltung');
 
@@ -52,12 +53,29 @@ veranstaltungsController.get('/veranstaltung/show/:id', function (req, res) {
 veranstaltungsController.post('/veranstaltung/add',function (req, res) {
 
     let veranstaltungInstance = new Veranstaltung(req.body);
-
+    let  transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "f91515824063a2",
+            pass: "e0703dbc730281"
+        }
+    });
     console.log(veranstaltungInstance);
 
     veranstaltungInstance.save((err, doc) =>{
         if (!err){
-            res.send('Veranstaltung wurde  erfolgreich erstellt!');}
+            res.send('Veranstaltung wurde erfolgreich erstellt!');
+            transport.sendMail({
+                from: 'test@vms.de',
+                to: 'test@kunde.de' ,
+                subject: 'Test Mail 1',
+                text: 'Herzlich Willkommen bei VMS'
+            })
+
+            }
+
+
         else  {console.log(err.toString());
             res.status(500).send(err.toString()); }
 
