f6d0de1809611915f6ad41c905e7fbb0dc057337
dorianprobstmeier <dorian@probstmeier.de>
2020-12-25 15:06:01 +0100

    admin hinzugefügt

---
+++ app/controllers/admin.controller.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const adminController = express();
+const Admin = require('../models/Admin');
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
+//Registration
+
+adminController.post('/admin/registration/add', function (req, res) {
+    const { email, password, password2 } = req.body;
+    let errors = [];
+
+    if (!email || !password || !password2) {
+        errors.push({ msg: 'Please enter all fields' });
+    }
+
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    if (errors.length > 0) {
+        res.send({
+            errors,
+            email,
+            password,
+            password2
+        });
+    } else {
+        const newAdmin = new Admin({
+            email,
+            password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newAdmin.password, salt, (err, hash) => {
+                if (err) throw err;
+                newAdmin.password = hash;
+                newAdmin
+                    .save((err, doc) => {
+                        const token = newAdmin.generateJwt();
+                        if (!err){
+                            res.json({
+                                "token": token
+                            })
+                        }
+                        else  {console.log(err.toString());
+                            res.status(500).send(err.toString()); }
+                    })
+
+                req.flash('success_msg', 'Du bist nun registriert')
+                console.log(newAdmin);
+            });
+        });
+    }
+});
+
+
+//login
+adminController.post('/admin/login', (req, res, next) =>{
+
+    let fetchedUser;
+
+    Admin.findOne({email:req.body.email}).then(function(admin){
+        if(!admin){
+            return res.status(401).json({message: 'Login Failed, no such User!'})
+        }
+        fetchedUser=admin;
+        return bcrypt.compare(req.body.password, admin.password);
+    }).then (result => {
+        console.log(fetchedUser)
+        if (!result) {
+            return res.status(401).json({message: 'Login failed: wrong password!'})
+        }
+        else {
+
+            const token = jwt.sign(
+                {email: fetchedUser.email, userID: fetchedUser._id}, "private_key",
+                {expiresIn: "1h"}
+            );
+            res.status(200).json({
+                token: token,
+                expiresIn: 3600,
+                userID: fetchedUser._id
+            });
+            console.log('logged in!')
+        }
+
+    })
+        .catch(e=>{
+            console.log(e)
+        })
+})
+
+module.exports = adminController;
