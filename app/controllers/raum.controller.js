4715d7ac1827b713c63b048b4d49f3426da3c625
Klaus <klaus-fischer96@gmx.de>
2020-12-31 14:31:48 +0100

    availability check for room; enter room and date, get availability

--- app/controllers/raum.controller.js
+++ app/controllers/raum.controller.js
@@ -1,3 +1,6 @@
+const Moment = require('moment');
+const MomentRange = require('moment-range');
+const moment = MomentRange.extendMoment(Moment);
 const express = require('express'),
     raumController = express();
 
@@ -97,23 +100,41 @@ raumController.put('/raum/edit/:id',function (req, res, next) {
 
 // verfügbarkeit prüfen
 
-raumController.post('/raum/verfuegbarkeit/:id', function (req, res){
-    let userInputDatum = req.body;
-    let veranstaltungStart = '2020-12-19'; // der eine Tag dazwischen fehlt wenn zb am 20 der Raum nur für diesen Tag angefordert wird
-    let veranstaltungEnde = '2020-12-21';
+raumController.post('/raum/verfuegbarkeit/:id', function (req, res) {
+    let foundevents;
+    let range1;
+    let range2;
+    let result2;
+    result2 = false;
 
-    let raumInstance = Raum.findById({_id: req.params.id});
-    //finde Veranstaltungen von diesem Raum
-    console.log(raumInstance);
+    Veranstaltung.find({raum: req.body.raum}, 'start_datum end_datum raum', function (err, veranstaltung) { //this looks up all events where the specified room is assigned to
 
-    console.log((userInputDatum.startDatum));
-    console.log(veranstaltungStart);
-    if( ((userInputDatum.startDatum === veranstaltungStart) || (userInputDatum.startDatum === veranstaltungEnde)) || ((userInputDatum.endDatum === veranstaltungStart) || (userInputDatum.endDatum === veranstaltungEnde)) ){
-        res.send('Raum ist nicht verfügbar.');
-    }
-    else {
-        res.send('Raum ist verfügbar.');
-    }
+        if (veranstaltung.length !==0) {
+            foundevents = veranstaltung //saves all found events as arrays to foundevents
+            //foundevents.forEach(event => { //as long as result is false there is no overlap; complete array of matching rooms is searched until overlap is found or end of array is reached
+            foundevents.every(event => {
+                range1 = moment.range(req.body.start_datum, req.body.end_datum) //dates to check are passed in
+                range2 = moment.range(event.start_datum, event.end_datum) //these are the dates of the exisiting event
+                result2 = range1.overlaps(range2) //ranges are checked for overlap; result 2 is updated
+                console.log(foundevents.start_datum)
+                console.log(req.body.start_datum)
+                if (result2 !== false) { //if there is an overlap, break
+                    return false;
+                }
+                if (result2 === undefined) { //if result2 is undefined, break
+                    return false;
+                }
+
+
+            })
+            if (result2 === false) { //if overlap is not existing, room is available
+                res.send("Der Raum " + req.body.raum + " ist im gewünschten Zeitraum verfügbar.")
+            } else {//if overlap is true, room is not available
+                res.send("Der Raum " + req.body.raum + " ist im gewünschten Zeitraum nicht verfügbar. Bitte versuchen Sie ein anderes Datum oder einen andern Raum.")
+            }
+        }
+        else {res.status(500).send("Dieser Raum existiert nicht!")}//if room is not found, send error msg
+        });
 });
 
 
