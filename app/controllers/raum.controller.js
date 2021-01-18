be5a5bcab79908b8d3df11bc9338276c4879ec54
dorianprobstmeier <dorian@probstmeier.de>
2021-01-18 12:49:19 +0100

    fixed raumverfügbarkeit

--- app/controllers/raum.controller.js
+++ app/controllers/raum.controller.js
@@ -104,40 +104,44 @@ raumController.put('/raum/edit/:id',function (req, res, next) {
 // verfügbarkeit prüfen
 
 raumController.post('/raum/verfuegbarkeit/:id', function (req, res) {
-    let foundevents;
+    let veranstaltung;
     let range1;
     let range2;
     let result2;
     result2 = false;
-
-    Veranstaltung.find({raum: req.body.raum}, 'start_datum end_datum raum', function (err, veranstaltung) { //this looks up all events where the specified room is assigned to
-
-        if (veranstaltung.length !==0) {
-            foundevents = veranstaltung //saves all found events as arrays to foundevents
-            //foundevents.forEach(event => { //as long as result is false there is no overlap; complete array of matching rooms is searched until overlap is found or end of array is reached
-            foundevents.every(event => {
-                range1 = moment.range(req.body.start_datum, req.body.end_datum) //dates to check are passed in
-                range2 = moment.range(event.start_datum, event.end_datum) //these are the dates of the exisiting event
-                result2 = range1.overlaps(range2) //ranges are checked for overlap; result 2 is updated
-                console.log(foundevents.start_datum)
-                console.log(req.body.start_datum)
-                if (result2 !== false) { //if there is an overlap, break
-                    return false;
+    Raum.findOne({_id: req.params.id}, function (err, raum){
+        if(err){
+            res.status(500).json({message: 'Der Raum existiert nicht!'})
+        }else{
+            let raumNummer = raum.raumNr;
+            console.log('test' + raumNummer)
+            Veranstaltung.find({raum: raumNummer}, function (err, veranstaltung){
+                console.log(veranstaltung)
+                if(err){
+                    res.status(200).json({message: 'Der Raum ' + raumNummer + ' ist im gewünschten Zeitraum verfügbar.'});
+                }else{
+                    veranstaltung.every(event => {
+                            range1 = moment.range(req.body.start_datum, req.body.end_datum) //dates to check are passed in
+                            range2 = moment.range(event.start_datum, event.end_datum) //these are the dates of the exisiting event
+                            result2 = range1.overlaps(range2) //ranges are checked for overlap; result 2 is updated
+                            console.log(veranstaltung.start_datum)
+                            console.log(req.body.start_datum)
+                            if (result2 !== false) { //if there is an overlap, break
+                                return false;
+                            }
+                            if (result2 === undefined) { //if result2 is undefined, break
+                                return false;
+                            }
+                    })
+                        if (result2 === false) { //if overlap is not existing, room is available
+                            res.status(200).json({message: 'Der Raum ' + raumNummer + ' ist im gewünschten Zeitraum verfügbar.'})
+                        } else {//if overlap is true, room is not available
+                            res.status(200).json({message: "Der Raum " + raumNummer + " ist im gewünschten Zeitraum nicht verfügbar. Bitte versuchen Sie ein anderes Datum oder einen andern Raum."})
+                        }
                 }
-                if (result2 === undefined) { //if result2 is undefined, break
-                    return false;
-                }
-
-
-            })
-            if (result2 === false) { //if overlap is not existing, room is available
-                res.send("Der Raum " + req.body.raum + " ist im gewünschten Zeitraum verfügbar.")
-            } else {//if overlap is true, room is not available
-                res.send("Der Raum " + req.body.raum + " ist im gewünschten Zeitraum nicht verfügbar. Bitte versuchen Sie ein anderes Datum oder einen andern Raum.")
-            }
+            });
         }
-        else {res.status(500).send("Dieser Raum existiert nicht!")}//if room is not found, send error msg
-        });
+    })
 });
 
 
